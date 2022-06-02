4fd2510ce0fe337ada84aa50a3f8afc521fca9dc
KarineMardoyan <105147872+KarineMardoyan@users.noreply.github.com>
2022-06-02 16:45:14 +0400

    Add files via upload

---
+++ evenNumbers.js
@@ -0,0 +1,7 @@
+var numbers = [1, 2, 3, 4, 5, 6, 80];
+        for (let i = 0; i < 31; i++) {
+            if (numbers[i] % 2 == 0) {
+                document.write(numbers[i] + "<br>");
+            }
+            else;
+        }
